c1300c97c80ec8df66baa9f45d1c76768880a05e
Aditya Swami <adityaswami2004@gmail.com>
2024-08-13 18:59:28 +0530

    Added test_pyls.py for unit tests

---
+++ test_pyls.py
@@ -0,0 +1,30 @@
+import pytest
+from pyls import getDescriptionsOfFilesInDir, formatResults, printResults
+
+
+def test_getDescriptionsOfFilesInDir(self):
+    """
+    Unit test for func getDescriptionsOfFilesInDir() from pyls
+
+    :return: Boolean value, test pass/fail
+    """
+    return True
+
+
+def test_formatResults(self):
+    """
+    Unit test for func formatResults() from pyls
+    
+    :return: Boolean value, test pass/fail
+    """
+    return True
+
+
+def test_printResults(self):
+    """
+    Unit test for func printResults() from pyls
+    
+    :return: Boolean value, test pass/fail
+    """
+    return True
+    
